03be0c1fe1554b5a350923ca986ac36becfdcbda
Samuel Klopsch <sakl5022@colorado.edu>
2015-12-02 14:38:03 -0700

    Revert "Added important changes to enemy and platform handlers"
    This reverts commit a33f653ee3eae928e50ce36a4f6c560669157dec.

--- Moodle_Jump/lib/Enemy.java
+++ Moodle_Jump/lib/Enemy.java
@@ -1,21 +1,10 @@
 package lib;
 
 public class Enemy {
-	private double[] velocity;
 	private int[] position;
-	private int status;
+	private double[] velocity;
 	private int type;
-	
-	public Enemy(int[] position, int type) {
-		this.position = position;
-		this.type = type;
-		
-		switch(type) {
-		case 0:
-			this.status = 1;
-			break;
-		}
-	}
+	private int status;
 	
 	public int[] getPosition() {
 		return this.position;
@@ -29,7 +18,7 @@ public class Enemy {
 	public int getType() {
 		return this.type;
 	}
-	public void update(int[] player_position) {
+	public void update(int player_position) {
 		
 	}
 }
--- Moodle_Jump/lib/Platform.java
+++ Moodle_Jump/lib/Platform.java
@@ -1,28 +1,13 @@
 package lib;
 
 public class Platform {
-	private double[] velocity;
-	private int height;
 	private int[] position;
+	private double[] velocity;
 	private int status;
 	private int type;
+	private int height;
 	private int width;
 	
-	public Platform(int height, int[] position, int type, int width) {
-		this.height = height;
-		this.position = position;
-		this.type = type;
-		this.width = width;
-		
-		switch(type) {
-			// Initial platform with status of -1 to signify infinite health
-			case 0: this.status = -1;
-				break;
-			// Platform with status of 2 means that it can be jumped off of twice before disappearing
-			case 1: this.status = 2;
-				break;
-		}
-	}
 	public int[] getPosition() {
 		return this.position;
 	}
@@ -35,7 +20,13 @@ public class Platform {
 	public int getType() {
 		return this.type;
 	}
-	public void update(int[] player_position) {
+	public int getHeight() {
+		return this.height;
+	}
+	public int getWidth() {
+		return this.width;
+	}
+	public void update() {
 		
 	}
 }
